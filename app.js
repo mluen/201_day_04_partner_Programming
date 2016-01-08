11111c18740269290dbd599d40b8bddc517de775
Ben Garnaat <garnaat.ben@gmail.com>
2016-01-07 16:15:23 -0800

    updated README.  Added question six.  Updated formatting.

--- app.js
+++ app.js
@@ -3,29 +3,32 @@ var res2 = document.getElementById('result2');
 var res3 = document.getElementById('result3');
 var res4 = document.getElementById('result4');
 var res5 = document.getElementById('result5');
-var total = document.getElementById('total');
+var res6 = document.getElementById('result6');
 var total1 = document.getElementById('total1');
 var total2 = document.getElementById('total2');
-
-var user = prompt('Welcome, please enter a name: ');
-console.log('User name input:  ' + user);
-alert('Welcome ' + user);
+var total3 = document.getElementById('total3');
 
 var counterCorrect = 0;
 var counterTotal = 0;
 var summary;
+
+var user = prompt('Welcome, please enter a name: ');
+console.log('User name input:  ' + user);
+alert('Welcome ' + user);
 
 questionOne();
 questionTwo();
 questionThree();
 questionFour();
 questionFive();
+questionSix();
+
 
 function questionOne() {
-  var answer1 = prompt('Question 1:  True or False, Ben was born in Seattle, Washington?');
+  var answer1 = prompt('Question 1:  True or False, Ben was born in Seattle, Washington?').toLowerCase();
   console.log('User response to Question 1:  ' + answer1);
   counterTotal++;
-  if (answer1.toLowerCase() == 'false') {
+  if (answer1 == 'false') {
     res1.textContent = 'Correct, ' + user + '!  Ben was born in Tucson, Arizona.';
     counterCorrect++;
   } else {
@@ -34,13 +37,12 @@ function questionOne() {
 }
 
 
-
 function questionTwo() {
   // accept Y/y/Yes/yes as valid input
-  var answer2 = prompt('Question 2:  Does Ben like food? (Enter "Y" or "N")');
+  var answer2 = prompt('Question 2:  Does Ben like food? (Enter "Y" or "N")').toLowerCase();
   console.log('User response to Question 2:  ' + answer2);
   counterTotal++;
-  if ((answer2.toLowerCase() === "y") || (answer2.toLowerCase() === "yes")) {
+  if ((answer2 === "y") || (answer2 === "yes")) {
     res2.textContent = 'Correct, ' + user + '!  Ben does indeed like food.';
     counterCorrect++;
   } else {
@@ -48,17 +50,20 @@ function questionTwo() {
   }
 }
 
+
 function questionThree() {
-  var answer3 = prompt('Question 3:  What was Ben\'s first Pet? (Enter "Dog", "Cat", "Bird", "Fish", "Velociraptor")')
+  var answer3 = prompt('Question 3:  What was Ben\'s first Pet? (Enter "Dog", "Cat", "Bird", "Fish", "Velociraptor")').toLowerCase()
   console.log('User resonse to Question 3:  ' + answer3);
   counterTotal++;
-  if (answer3.toLowerCase() == "fish") {
+  if (answer3 == "fish") {
     res3.textContent = 'Correct, ' + user + '!  Ben\'s first pet was a fish.';
     counterCorrect++;
   } else {
     res3.textContent = 'Sorry ' + user + ', as much as Ben wanted a ' + answer3 + ' his parents wouldn\'t allow something like that.';
   }
 }
+
+
 function questionFour() {
   var answer4 = prompt('Question 4:  Pick a number between 1 and 10:');
   while ((isNaN(answer4)) || (answer4 < 1) || (answer4 > 10)) {
@@ -75,6 +80,8 @@ function questionFour() {
     res4.textContent = 'Sorry ' + user + ', your guess was too low.';
   }
 }
+
+
 function questionFive() {
   var answer5 = prompt('Question 5:  Pick a number between 1 and 10:');
   counterTotal++;
@@ -94,7 +101,37 @@ function questionFive() {
   counterCorrect++;
 }
 
-summary = (counterCorrect / counterTotal) * 100;
-total.textContent = 'Total Correct:  ' + counterCorrect;
-total1.textContent = ' Total Questions:  ' + counterTotal;
-total2.textContent = 'Score:  ' + summary + '%';
+
+function questionSix() {
+  var answer6 = prompt('Question 6: Name a planet in the Solar System:').toLowerCase();
+  console.log('User planet:' + answer6);
+  counterTotal++;
+  var planets = [
+    'mercury',
+    'venus',
+    'earth',
+    'mars',
+    'jupiter',
+    'saturn',
+    'uranus',
+    'neptune',
+    'pluto'
+  ];
+  var yeahPlanet = false;
+  for (i = 0; i < planets.length; i++) {
+    if (answer6 == planets[i]) {
+      res6.textContent = 'Correct, ' + user + '!  ' + answer6 + ' is a planet.';
+      i = planets.length;  // exit loop
+      counterCorrect++;
+      yeahPlanet = true;
+    }
+    if (yeahPlanet == false) {
+      res6.textContent = 'Sorry, ' + user + ', ' + answer6 + ' is not a planet.';
+    }
+  }
+
+  summary = (counterCorrect / counterTotal) * 100;
+  total1.textContent = 'Total Correct:  ' + counterCorrect;
+  total2.textContent = ' Total Questions:  ' + counterTotal;
+  total3.textContent = 'Score:  ' + summary + '%';
+}
